389440e9282d4276709335ea2839b73e291c900e
shenzhe <shenzhe163@gmail.com>
2016-01-22 10:02:37 +0800

    first

--- config.php
+++ config.php
@@ -1,118 +1,20 @@
 <?php
 return [
-    'laya-hook'=>[
-        'refs/heads/master'=> [
+    'test'=>[                                    //项目名
+        'refs/heads/dev'=> [                            //分支名称
             [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-hook',
-            ]
-        ]
-    ],
-    'laya-ask'=>[
-        'refs/heads/master'=> [  //主分支，对应于线上的数据
-            [
-                'host'=>'ask.layabox.com',
-                'port'=>8999,
-                'path'=> '/data/wwwroot/ask.layabox.com',
-            ]
-        ],
-        'refs/heads/dev'=> [
-            [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-ask',
-            ]
-        ]
-    ],
-    'laya-sso'=>[
-        'refs/heads/master'=> [  //主分支，对应于线上的数据
-            [
-                'host'=>'sso.layabox.com',
-                'port'=>8999,
-                'path'=> '/data/wwwroot/sso.layabox.com',
-            ]
-        ],
-        'refs/heads/dev'=> [
-            [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-sso',
-            ]
-        ]
-    ],
-    'laya-ucenter'=>[
-        'refs/heads/master'=> [  //主分支，对应于线上的数据
-            [
-                'host'=>'sso.layabox.com',
-                'port'=>8999,
-                'path'=> '/data/wwwroot/ucenter.layabox.com',
-            ]
-        ],
-        'refs/heads/dev'=> [
-            [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-ucenter',
-            ]
-        ]
-    ],
-    'laya-lib'=>[
-        'refs/heads/master'=> [  //主分支，对应于线上的数据
-            [
-                'host'=>'sso.layabox.com',
-                'port'=>8999,
-                'path'=> '/data/wwwroot/laya-lib',
+                'host'=>'127.0.0.1',                        //分支所属服务器ip
+                'port'=>8999,                               //分支所属服务器端口
+                'path'=> '/home/shenzhe/test',
             ],
         ],
-        'refs/heads/dev'=> [
+        'refs/heads/master'=> [                            //分支名称
             [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-lib',
+                'host'=>'192.168.0.1',                        //分支所属服务器ip
+                'port'=>8999,                                   //分支所属服务器端口
+                'path'=> '/home/shenzhe/test',
             ]
         ]
     ],
-    'laya-open'=>[
-//        'refs/heads/master'=> [  //主分支，对应于线上的数据
-//            'host'=>'sso.layabox.com',
-//            'port'=>8999,
-//            'path'=> '/data/wwwroot/laya-open',
-//        ],
-        'refs/heads/dev'=> [
-            [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-open',
-            ]
-        ]
-    ],
-    'laya-open-sdk'=>[
-//        'refs/heads/master'=> [  //主分支，对应于线上的数据
-//            'host'=>'sso.layabox.com',
-//            'port'=>8999,
-//            'path'=> '/data/wwwroot/laya-open',
-//        ],
-        'refs/heads/dev'=> [
-            [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-open-sdk',
-            ]
-        ]
-    ],
-    'laya-bbs'=>[
-//        'refs/heads/master'=> [  //主分支，对应于线上的数据
-//            'host'=>'sso.layabox.com',
-//            'port'=>8999,
-//            'path'=> '/data/wwwroot/laya-open',
-//        ],
-        'refs/heads/dev'=> [
-            [
-                'host'=>'127.0.0.1',
-                'port'=>8999,
-                'path'=> '/home/git/laya-bbs',
-            ]
-        ]
-    ],
+
 ];
